6989d7a3ec85ce1408436e7eaa850b1dda8861cf
Alexandre <ryxandy@hotmail.com>
2021-09-05 12:12:04 -0300

    Added a table called User to update the way that u can log into the system, for now its a hardcoded login check

--- src/main/java/com/ryxandy/pokedex/Controller/BasicController.java
+++ src/main/java/com/ryxandy/pokedex/Controller/BasicController.java
@@ -1,7 +1,9 @@
 package com.ryxandy.pokedex.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BasicController {
@@ -16,6 +18,18 @@ public class BasicController {
     String home(){
         System.out.println("Entrou no Home");
         return "Home";
+    }
+
+    @PostMapping("/loginForm")
+    String processLogin(Model model, String username, String password){
+        System.out.println(username);
+        if (username.equals("ash") && password.equals("88719588")){
+            System.out.println("Logado com sucesso");
+            return "Home";
+        }
+        System.out.println("Seu login falhou");
+        model.addAttribute("failLogin",1 );
+        return "Login";
     }
 
 
--- src/main/java/com/ryxandy/pokedex/Model/Pokemon.java
+++ src/main/java/com/ryxandy/pokedex/Model/Pokemon.java
@@ -1,4 +1,76 @@
 package com.ryxandy.pokedex.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Pokemon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    private String name;
+
+    private String type;
+
+    private String weakness;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pokemon pokemon = (Pokemon) o;
+        return id == pokemon.id && Objects.equals(name, pokemon.name) && Objects.equals(type, pokemon.type) && Objects.equals(weakness, pokemon.weakness);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type, weakness);
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", weakness='" + weakness + '\'' +
+                '}';
+    }
 }
